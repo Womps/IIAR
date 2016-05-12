1e2e64b20f13e109bc093d6b3fb784aba531bfaf
Romain EPIARD <Romain EPIARD>
2016-05-12 16:29:01 +0200

    Create application.java with main

---
+++ src/com/iiar/application/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.iiar.application;
+
+/**
+ * @author Romain EPIARD
+ *
+ */
+public class Application
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+}
